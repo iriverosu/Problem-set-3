dd032357ce8186e8269b39b3116112d8c7944bee
luifercue <112282878+luifercue@users.noreply.github.com>
2022-11-02 16:11:57 -0500

    .

--- scripts/PS3.R
+++ scripts/PS3.R
@@ -1,7 +1,9 @@
-skim(train)
-
 install.packages("tidyverse")
 install.packages("pacman")
+install.packages("osmdata")
+install.packages("httr2")
+library(httr2)
+library(osmdata)
 library(pacman)
 require(pacman)
 p_load(here,knitr,tidyverse,ggthemes,fontawesome,kableExtra)
@@ -9,26 +11,31 @@ p_load(tidyverse,rio,viridis,sf, leaflet, tmaptools)
 # option html
 options(htmltools.dir.version = F)
 opts_chunk$set(fig.align="center", fig.height=4 , dpi=300 , cache=F)
+remotes::install_github('ropensci/osmdata')
 
 
 #Variables PS3 
-  #1.Área total o cubierta
-  #2.bedrooms
-  #3.bathrooms (k-vecinos)
-  #4.property type
-  #5.Distancia al CBD
-  #6.Distancia Tansporte (estaciones o vías arteriales)
-  #7.Distancia Colegios o universidades 
-  #8.IPM #coordenadas
+#1.Área total o cubierta
+#2.bedrooms
+#3.bathrooms (k-vecinos)
+#4.property type
+#5.Distancia al CBD
+#6.Distancia Tansporte (estaciones o vías arteriales)
+#7.Distancia Colegios, universidades, kindergarden
+#8.IPM #coordenadas
 
 #data como sf
 train2 <- st_as_sf(x = train, ## datos
                    coords=c("lon","lat"), ## coordenadas
                    crs=4326) ## CRS
+
+leaflet() %>% addTiles() %>% addCircleMarkers(data=train2)
+class(houses)
 class(train2)
 table(train2$rooms, train2$bedrooms)
 sum(is.na(train2$bedrooms))
 sum(is.na(train2$bathrooms)) #15032
+
 
 #------------------------------------------------------------- OSM ------------------------------------------------------------  
 ## Buscar un lugar público por el nombre
@@ -41,8 +48,79 @@ cbd
 #1. total=cubierta
 #2. Texto 
 #------------------------------------------------------------- CBD -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------  
-Isabella
+Isabella 
 # Bgtá (https://www.larepublica.co/economia/concentracion-de-trabajadores-no-es-proporcional-respecto-a-las-zonas-de-residencia-3453320)
+## geocode_OSM no reconoce el caracter #, en su lugar se usa %23% 
+cbd <- geocode_OSM("Centro Internacional, Bogotá", as.sf=T) 
+cbd
+
+####################### commercial Bgtá
+opq(bbox = getbb("Bogotá Colombia"))
+## Guardamos un amenity con el Poligono que en este caso es bogota con los coelgios 
+osm5 = opq(bbox = getbb("Bogotá Colombia")) %>%
+  add_osm_feature(key="building" , value="commercial") 
+## Fijamos un objeto
+osm_sf5 = osm5 %>% osmdata_sf()
+osm_sf5
+commercial_bog = osm_sf5$osm_points %>% select(osm_id,amenity) 
+commercial_bog
+### Si queremos visualizarlo
+leaflet() %>% addTiles() %>% addCircleMarkers(data=commercial_bog , col="red")
+
+####################### Office Bgtá
+opq(bbox = getbb("Bogotá Colombia"))
+## Guardamos un amenity con el Poligono que en este caso es bogota con los coelgios 
+osm6 = opq(bbox = getbb("Bogotá Colombia")) %>%
+  add_osm_feature(key="building" , value="office") 
+## Fijamos un objeto
+osm_sf6 = osm6 %>% osmdata_sf()
+osm_sf6
+office_bog = osm_sf6$osm_points %>% select(osm_id,building) 
+office_bog
+### Si queremos visualizarlo
+leaflet() %>% addTiles() %>% addCircleMarkers(data=office_bog , col="red")
+
+####################### Industria Bgtá
+opq(bbox = getbb("Bogotá Colombia"))
+## Guardamos un amenity con el Poligono que en este caso es bogota con los coelgios 
+osm9 = opq(bbox = getbb("Bogotá Colombia")) %>%
+  add_osm_feature(key="building" , value="industrial") 
+## Fijamos un objeto
+osm_sf9 = osm9 %>% osmdata_sf()
+osm_sf9
+industrial_bog = osm_sf9$osm_points %>% select(osm_id,building) 
+industrial_bog
+### Si queremos visualizarlo
+leaflet() %>% addTiles() %>% addCircleMarkers(data=industrial_bog , col="red")
+
+#Medellín
+#industria
+opq(bbox = getbb("Medellín Colombia"))
+## Guardamos un amenity con el Poligono que en este caso es bogota con los coelgios 
+osm7 = opq(bbox = getbb("Medellín Colombia")) %>%
+  add_osm_feature(key="building" , value="industrial") 
+## Fijamos un objeto
+osm_sf7 = osm7 %>% osmdata_sf()
+osm_sf7
+industrial_med = osm_sf7$osm_points %>% select(osm_id,building) 
+industrial_med
+### Si queremos visualizarlo
+leaflet() %>% addTiles() %>% addCircleMarkers(data=industrial_med , col="red")
+
+####################### Office Bgtá
+opq(bbox = getbb("Medellín Colombia"))
+## Guardamos un amenity con el Poligono que en este caso es bogota con los coelgios 
+osm8 = opq(bbox = getbb("Medellín Colombia")) %>%
+  add_osm_feature(key="building" , value="office") 
+## Fijamos un objeto
+osm_sf8 = osm8 %>% osmdata_sf()
+osm_sf8
+office_med =  osm_sf8$osm_points %>% select(osm_id,building) 
+office_med
+### Si queremos visualizarlo
+leaflet() %>% addTiles() %>% addCircleMarkers(data=office_med , col="red")
+
+
 # Medellín (https://www.artchitectours.es/tour/el-poblado-medellin/)
 # Cali (Torre de Cali 1.5km medialuna)
 #-------------------------------------------------- Transporte (vías y estaciones) ----------------------------------------------------------------------------------------------------------------------- 
@@ -51,10 +129,78 @@ Daniel
 # Medellín 
 # Cali 
 #---------------------------------------------------- Colegios/ Universidades  ---------------------------------------------------------------------------------------------------------------------- 
-Luisa
-# Bgtá
-# Medellín 
+####################### Colegios Bgtá
+opq(bbox = getbb("Bogotá Colombia"))
+## Guardamos un amenity con el Poligono que en este caso es bogota con los coelgios 
+osm = opq(bbox = getbb("Bogotá Colombia")) %>%
+  add_osm_feature(key="amenity" , value="school") 
+## Fijamos un objeto
+osm_sf = osm %>% osmdata_sf()
+osm_sf
+colegios_bog = osm_sf$osm_points %>% select(osm_id,amenity) 
+colegios_bog
+### Si queremos visualizarlo
+leaflet() %>% addTiles() %>% addCircleMarkers(data=colegios_bog , col="red")
+
+####################### Universidades Bgtá
+## Guardamos un amenity con el Poligono que en este caso es bogota con los coelgios 
+osm2 = opq(bbox = getbb("Bogotá Colombia")) %>%
+  add_osm_feature(key="amenity" , value="university") 
+## Fijamos un objeto
+osm_sf2 = osm2 %>% osmdata_sf()
+osm_sf2 #2377 puntos y 153 poligonos
+universidades_bog = osm_sf2$osm_points %>% select(osm_id,amenity) 
+universidades_bog
+### Si queremos visualizarlo
+leaflet() %>% addTiles() %>% addCircleMarkers(data=universidades_bog , col="black") 
+####################### Colegios Medellín
+## Guardamos un amenity con el Poligono que en este caso es bogota con los coelgios 
+osm3 = opq(bbox = getbb("Medellín Colombia")) %>%
+  add_osm_feature(key="amenity" , value="school") 
+## Fijamos un objeto
+osm_sf3 = osm3 %>% osmdata_sf()
+osm_sf3
+colegios_med = osm_sf3$osm_points %>% select(osm_id,amenity) 
+colegios_med
+### Si queremos visualizarlo
+leaflet() %>% addTiles() %>% addCircleMarkers(data=colegios_med , col="red")
+
+####################### Universidades Medellìn
+## Guardamos un amenity con el Poligono que en este caso es bogota con los coelgios 
+osm4 = opq(bbox = getbb("Medellín Colombia")) %>%
+  add_osm_feature(key="amenity" , value="university") 
+## Fijamos un objeto
+osm_sf4 = osm4 %>% osmdata_sf()
+osm_sf4 
+universidades_med = osm_sf4$osm_points %>% select(osm_id,amenity) 
+universidades_med
+### Si queremos visualizarlo
+leaflet() %>% addTiles() %>% addCircleMarkers(data=universidades_med , col="black")
+
 # Cali 
+####################### Colegios cali
+## Guardamos un amenity con el Poligono que en este caso es bogota con los coelgios 
+osm3 = opq(bbox = getbb("Medellín Colombia")) %>%
+  add_osm_feature(key="amenity" , value="school") 
+## Fijamos un objeto
+osm_sf3 = osm3 %>% osmdata_sf()
+osm_sf3
+colegios_med = osm_sf3$osm_points %>% select(osm_id,amenity) 
+colegios_med
+### Si queremos visualizarlo
+leaflet() %>% addTiles() %>% addCircleMarkers(data=colegios_med , col="red")
+####################### Universidades cali
+## Guardamos un amenity con el Poligono que en este caso es bogota con los coelgios 
+osm4 = opq(bbox = getbb("Medellín Colombia")) %>%
+  add_osm_feature(key="amenity" , value="university") 
+## Fijamos un objeto
+osm_sf4 = osm4 %>% osmdata_sf()
+osm_sf4 
+universidades_med = osm_sf4$osm_points %>% select(osm_id,amenity) 
+universidades_med
+### Si queremos visualizarlo
+leaflet() %>% addTiles() %>% addCircleMarkers(data=universidades_med , col="black")
+
 #------------------------------------------------------ Bathrooms (k-vecinos)  ----------------------------------------------------------------------------------------------------------------------- 
 # Bgtá
 # Medellín 
